b84e057949c42d421ac89592d9e6c70365655c30
SunshineAI <2273766063@qq.com>
2025-02-17 20:30:43 +0800

    Change To Cdebootstrap
    Changes to be committed:
    	modified:   .github/workflows/Build-Debian-RootFS-DistroBox-AMD64.yml
    	modified:   .github/workflows/Build-Debian-RootFS-DistroBox-Bullseye-AMD64.yml
    	modified:   .github/workflows/Build-Debian-RootFS-Xfce-AMD64.yml
    	modified:   .github/workflows/Build-Debian-RootFS-Xfce-Bullseye-AMD64.yml
    	modified:   .github/workflows/Debian-AMD64-build/Build-Debian-RootFS-DistroBox-AMD64.yml
    	modified:   .github/workflows/Debian-AMD64-build/Build-Debian-RootFS-DistroBox-Bullseye-AMD64.yml
    	modified:   .github/workflows/Debian-AMD64-build/Build-Debian-RootFS-Xfce-AMD64.yml
    	modified:   .github/workflows/Debian-AMD64-build/Build-Debian-RootFS-Xfce-Bullseye-AMD64.yml
    	modified:   .github/workflows/Template/Build-Debian-ARM64-RootFS-Template.yml
    	modified:   chinese/dir_init.sh
    	modified:   english/dir_init.sh
    	modified:   scripts/micromamba-installer
    	modified:   scripts/mysql-installer

--- english/dir_init.sh
+++ english/dir_init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 创建英文目录结构
-mkdir -p "Graduation_Project_Materials/Opening_Report"
-mkdir -p "Graduation_Project_Materials/Thesis_Writing/Thesis_Writing_Standards_and_Templates"
-mkdir -p "Graduation_Project_Materials/Topic_Selection"
-mkdir -p "Graduation_Project_Materials/Guidelines_and_Schedule"
-mkdir -p "Opening_Report"
-mkdir -p "Thesis_Writing"
-mkdir -p "Topic_Selection"
-mkdir -p "Source_Code"
-mkdir -p "Guidelines_and_Schedule"
+sudo mkdir -vp "Graduation_Project_Materials/Opening_Report"
+sudo mkdir -vp "Graduation_Project_Materials/Thesis_Writing/Thesis_Writing_Standards_and_Templates"
+sudo mkdir -vp "Graduation_Project_Materials/Topic_Selection"
+sudo mkdir -vp "Graduation_Project_Materials/Guidelines_and_Schedule"
+sudo mkdir -vp "Opening_Report"
+sudo mkdir -vp "Thesis_Writing"
+sudo mkdir -vp "Topic_Selection"
+sudo mkdir -vp "Source_Code"
+sudo mkdir -vp "Guidelines_and_Schedule"
 
 echo "English directory creation completed!"
